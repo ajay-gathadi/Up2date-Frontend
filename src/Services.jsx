97864c7109c7de3e325776a0619846f67402c903
Ajay Gathadi <ajaygathadi@gmail.com>
2025-05-19 21:48:38 +0530

    Add Chip option to show selected services

--- src/Services.jsx
+++ src/Services.jsx
@@ -16,6 +16,8 @@ import {
   OutlinedInput,
   Menu,
   MenuItem,
+  Stack,
+  Chip,
 } from "@mui/material";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
@@ -189,35 +191,92 @@ function Services({ value, onChange, gender, error = null }) {
       onChange([...value, serviceId]);
     }
   };
-  const shouldAlwaysOpen = alwaysOpen || isOpen;
+
+  const getDisplayValue = () => {
+    if (value.length === 0) {
+      return "Select Services";
+    }
+
+    // return filteredServices
+    //   .filter((service) => value.includes(service.serviceId))
+    //   .map((service) => service.serviceName)
+    //   .join(", ");
+  };
+
+  const selectedServiceObject = useMemo(() => {
+    if (!services || services.length === 0) return [];
+    return value
+      .map((id) => filteredServices.find((service) => service.serviceId === id))
+      .filter(Boolean);
+  }, [value, filteredServices, services]);
+
+  const handleDeleteChip = (serviceIdToDelete) => {
+    onChange(value.filter((id) => id !== serviceIdToDelete));
+  };
 
   return (
-    <FormControl
-      ref={formControlRef}
-      fullWidth
-      margin="normal"
-      variant="outlined"
-      error={Boolean(error)}
-      sx={{
-        width: "400px",
-        position: "relative",
-      }}
-    >
-      <InputLabel
-        id="services-select-label"
-        shrink={true}
+    <>
+      <Stack
+        direction="row"
+        spacing={1}
+        sx={{ flexWrap: "wrap", gap: "4px", overflow: "auto" }}
+      >
+        {selectedServiceObject.map((currentService) => (
+          <Chip
+            key={currentService.serviceId}
+            label={currentService.serviceName}
+            onDelete={() => handleDeleteChip(currentService.serviceId)}
+            size="small"
+            sx={{
+              fontFamily: "Inter",
+              borderColor: "rgba(223, 168, 18, 0.69)",
+              "& .MuiOutlinedInput-input": {
+                display: "flex",
+                alignItems: "center",
+                padding: "2px 5px",
+                fontSize: "14px",
+                height: "auto",
+                cursor: alwaysOpen ? "default" : "pointer",
+              },
+              "& .MuiChip-label": {
+                color: "rgba(0,0,0,0.87",
+              },
+              "& .MuiChip-deleteIcon": {
+                color: "rgba(0,0,0,0.54)",
+                "&:hover": {
+                  color: "rgba(0,0,0,0.87)",
+                },
+              },
+            }}
+          />
+        ))}
+      </Stack>
+      <FormControl
+        ref={formControlRef}
+        fullWidth
+        margin="normal"
+        variant="outlined"
+        error={Boolean(error)}
         sx={{
-          fontFamily: "Inter",
-          color: error ? "error.main" : "black",
-          "&.Mui-focused": {
-            color: error ? "error.main" : "black",
-          },
+          width: "400px",
+          position: "relative",
         }}
       >
-        Services
-      </InputLabel>
+        <InputLabel
+          id="services-select-label"
+          shrink={true}
+          sx={{
+            fontFamily: "Inter",
+            color: error ? "error.main" : "black",
+            "&.Mui-focused": {
+              color: error ? "error.main" : "black",
+            },
+          }}
+        >
+          Services
+        </InputLabel>
 
-      {/* <Box
+        {/* <Box
       className="switch-container"
         sx={{
           position: "absolute",
@@ -240,202 +299,195 @@ function Services({ value, onChange, gender, error = null }) {
         />
       </Box> */}
 
-      <OutlinedInput
-        ref={outlinedInputRef}
-        id="services-select"
-        label="Services"
-        notched={true}
-        onClick={(e) => {
-          if (!alwaysOpen) {
-            toggleDropdown(e);
+        <OutlinedInput
+          ref={outlinedInputRef}
+          id="services-select"
+          label="Services"
+          notched={true}
+          onClick={(e) => {
+            if (!alwaysOpen) {
+              toggleDropdown(e);
+            }
+          }}
+          // placeholder="Select Services"
+          value={getDisplayValue()}
+          error={Boolean(error)}
+          readOnly
+          endAdornment={
+            !alwaysOpen && (
+              <Box
+                component="span"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  toggleDropdown(e);
+                  if (outlinedInputRef.current) {
+                    outlinedInputRef.current.focus();
+                  }
+                }}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  cursor: "pointer",
+                  padding: "4px",
+                  marginRight: "8px",
+                  borderRadius: "4px",
+                  "&:hover": {
+                    backgroundColor: "rgba(0, 0, 0, 0.1)",
+                  },
+                }}
+              >
+                {isOpen ? <ArrowUpward /> : <ArrowDownward />}
+              </Box>
+            )
           }
-        }}
-        error={Boolean(error)}
-        readOnly
-        endAdornment={
-          !alwaysOpen && (
-            <Box
-              component="span"
-              onClick={(e) => {
-                e.stopPropagation();
-                toggleDropdown(e);
-                if (outlinedInputRef.current) {
-                  outlinedInputRef.current.focus();
-                }
-              }}
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                cursor: "pointer",
-                padding: "4px",
-                marginRight: "8px",
-                borderRadius: "4px",
-                "&:hover": {
-                  backgroundColor: "rgba(0, 0, 0, 0.1)",
-                },
-              }}
-            >
-              {isOpen ? <ArrowUpward /> : <ArrowDownward />}
-            </Box>
-          )
-        }
-        value={
-          value.length > 0
-            ? `${value.length} ${
-                value.length === 1 ? "service selected" : "services selected"
-              }`
-            : "Select Services"
-        }
-        sx={{
-          fontFamily: "Inter",
-          // fontSize: "14px",
-          cursor: alwaysOpen ? "default" : "pointer",
-          transition: "all 0.2s ease",
-
-          "& .MuiOutlinedInput-root": {
+          sx={{
+            fontFamily: "Inter",
+            // fontSize: "14px",
             cursor: alwaysOpen ? "default" : "pointer",
-          },
+            transition: "all 0.2s ease",
 
-          "& .MuiOutlinedInput-input": {
-            fontSize: "14px",
-            textAlign: "center",
-            color: value.length === 0 ? "rgba(0,0,0,0.38)" : "inherit",
-            cursor: alwaysOpen ? "default" : "pointer",
-          },
+            "& .MuiOutlinedInput-root": {
+              cursor: alwaysOpen ? "default" : "pointer",
+            },
 
-          "& .MuiOutlinedInput-notchedOutline": {
-            borderColor: error ? "error.main" : "rgba(223, 168, 18, 0.69)",
-          },
+            "& .MuiOutlinedInput-input": {
+              fontSize: "14px",
+              textAlign: "center",
+              color: value.length === 0 ? "rgba(0,0,0,0.38)" : "inherit",
+              cursor: alwaysOpen ? "default" : "pointer",
+              textOverflow: "ellipsis",
+              overflow: "auto",
+              whiteSpace: "normal",
+            },
 
-          "&:hover .MuiOutlinedInput-notchedOutline": {
-            borderColor: error ? "error.main" : "rgba(223, 168, 18, 1)",
-          },
-
-          "&.Mui-focused": {
-            borderColor: "rgba(223, 168, 18, 0.05)",
             "& .MuiOutlinedInput-notchedOutline": {
               borderColor: error ? "error.main" : "rgba(223, 168, 18, 0.69)",
             },
-          },
-        }}
-      />
 
-      <Menu
-        anchorEl={anchorEl}
-        open={isOpen}
-        onClose={() => {
-          if (!alwaysOpen) {
-            setAnchorEl(null);
-            setIsOpen(false);
-          }
-        }}
-        keepMounted
-        disableAutoFocusItem
-        marginThreshold={0}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "center",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "center",
-        }}
-        slotProps={{
-          list: {
-            disablePadding: true,
-            onClick: (e) => e.stopPropagation(),
-            sx: {
-              maxHeight: "200px",
-              overflowY: "auto",
+            "&:hover .MuiOutlinedInput-notchedOutline": {
+              borderColor: error ? "error.main" : "rgba(223, 168, 18, 1)",
             },
-          },
-          paper: {
-            sx: {
-              mt: "4px",
-              height: "auto",
-              maxHeiht: "200px",
-              width: anchorEl ? `${anchorEl.offsetWidth}px` : "auto",
-              overflowY: "auto",
-              boxShadow: "0px 5px 15px rgba(0, 0, 0, 0.2)",
-              "&::-webkit-scrollbar": {
-                width: "8px",
-              },
 
-              "&::-webkit-scrollbar-track": {
-                backgroundColor: "rgba(155, 38, 38, 0.05)",
-                borderRadius: "4px",
+            "&.Mui-focused": {
+              borderColor: "rgba(223, 168, 18, 0.05)",
+              "& .MuiOutlinedInput-notchedOutline": {
+                borderColor: error ? "error.main" : "rgba(223, 168, 18, 0.69)",
               },
+            },
+          }}
+        />
 
-              "&::-webkit-scrollbar-thumb": {
-                backgroundColor: "rgba(0,0,0,0.2)",
-                borderRadius: "4px",
-                "&:hover": {
-                  backgroundColor: "rgba(0,0,0,0.58)",
+        <Menu
+          anchorEl={anchorEl}
+          open={isOpen}
+          onClose={handleClose}
+          keepMounted
+          disableAutoFocusItem
+          marginThreshold={0}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+          slotProps={{
+            list: {
+              disablePadding: true,
+              onClick: (e) => e.stopPropagation(),
+              sx: {
+                maxHeight: "200px",
+                overflowY: "auto",
+              },
+            },
+            paper: {
+              sx: {
+                mt: "4px",
+                height: "auto",
+                maxHeiht: "200px",
+                width: anchorEl ? `${anchorEl.offsetWidth}px` : "auto",
+                overflowY: "auto",
+                boxShadow: "0px 5px 15px rgba(0, 0, 0, 0.2)",
+                "&::-webkit-scrollbar": {
+                  width: "8px",
+                },
+
+                "&::-webkit-scrollbar-track": {
+                  backgroundColor: "rgba(155, 38, 38, 0.05)",
+                  borderRadius: "4px",
+                },
+
+                "&::-webkit-scrollbar-thumb": {
+                  backgroundColor: "rgba(0,0,0,0.2)",
+                  borderRadius: "4px",
+                  "&:hover": {
+                    backgroundColor: "rgba(0,0,0,0.58)",
+                  },
                 },
               },
             },
-          },
-        }}
-      >
-        {loading ? (
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              p: 2,
-            }}
-          >
-            <CircularProgress size={24} />
-          </Box>
-        ) : fetchError ? (
-          <MenuItem disabled>
-            <Alert severity="error" sx={{ mt: 1 }}>
-              Error loading services: {fetchError}
-            </Alert>
-          </MenuItem>
-        ) : filteredServices.length === 0 ? (
-          <MenuItem disabled>
-            <Alert severity="info" sx={{ mt: 1 }}>
-              No services available for {gender || "selected gender"}
-            </Alert>
-          </MenuItem>
-        ) : (
-          filteredServices.map((currentService) => (
-            <MenuItem
-              key={currentService.serviceId}
-              onClick={(e) => {
-                e.stopPropagation();
-                handleServiceChange(currentService.serviceId, e);
-              }}
-              disableRipple
+          }}
+        >
+          {loading ? (
+            <Box
               sx={{
-                fontFamily: "Inter",
-                fontSize: "14px",
                 display: "flex",
-                alignItems: "center",
-                transition: "all 0.2s ease",
-                "&:hover": {
-                  backgroundColor: "rgba(0, 0, 0, 0.1)",
-                },
+                justifyContent: "center",
+                p: 2,
               }}
             >
-              <Checkbox
-                checked={value.includes(currentService.serviceId)}
-                size="small"
+              <CircularProgress size={24} />
+            </Box>
+          ) : fetchError ? (
+            <MenuItem disabled>
+              <Alert severity="error" sx={{ mt: 1 }}>
+                Error loading services: {fetchError}
+              </Alert>
+            </MenuItem>
+          ) : filteredServices.length === 0 ? (
+            <MenuItem disabled>
+              <Alert severity="info" sx={{ mt: 1 }}>
+                No services available for {gender || "selected gender"}
+              </Alert>
+            </MenuItem>
+          ) : (
+            filteredServices.map((currentService) => (
+              <MenuItem
+                key={currentService.serviceId}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleServiceChange(currentService.serviceId, e);
+                }}
+                disableRipple
                 sx={{
-                  "&.Mui-checked": {
-                    color: "rgba(223, 168, 18, 0.69)",
+                  fontFamily: "Inter",
+                  fontSize: "14px",
+                  display: "flex",
+                  alignItems: "center",
+                  transition: "all 0.2s ease",
+                  "&:hover": {
+                    backgroundColor: "rgba(0, 0, 0, 0.1)",
                   },
                 }}
-              />
-              {currentService.serviceName}
-            </MenuItem>
-          ))
-        )}
-      </Menu>
+              >
+                <Checkbox
+                  checked={value.includes(currentService.serviceId)}
+                  size="small"
+                  sx={{
+                    "&.Mui-checked": {
+                      color: "rgba(223, 168, 18, 0.69)",
+                    },
+                  }}
+                />
+                {currentService.serviceName}
+              </MenuItem>
+            ))
+          )}
+        </Menu>
 
-      {/*       
+        {/*       
       <Collapse in={shouldAlwaysOpen} timeout="auto">
         {loading ? (
           <Box
@@ -521,7 +573,8 @@ function Services({ value, onChange, gender, error = null }) {
           </Paper>
         )}
       </Collapse> */}
-    </FormControl>
+      </FormControl>
+    </>
   );
 }
 
