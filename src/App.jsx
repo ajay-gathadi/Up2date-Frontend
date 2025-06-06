a6febe3d9273e4fdf3ca2029b6b177a57566715f
Ajay Gathadi <ajaygathadi@gmail.com>
2025-06-07 00:30:41 +0530

    Introduce navigation to multiple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,93 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import {
+  Navigate,
+  Route,
+  Routes,
+  BrowserRouter,
+  Router,
+} from "react-router-dom";
 import "./App.css";
 import Customer from "./Customer_Information/Customer.jsx";
-
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
+import AppDrawer from "./components/AppDrawer.jsx";
+import CustomerPage from "./Customer_Information/Customer.jsx";
+import DashboardPage from "./pages/DashBoard.jsx";
 function App() {
   // const [count, setCount] = useState(0)
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setOpenDrawer(!openDrawer);
+  };
+
+  const drawerContent = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Up2date Family Salon
+      </Typography>
+    </Box>
+  );
 
   return (
-    <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      <Customer />
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-    </>
+    <BrowserRouter>
+      <Box sx={{ display: "flex", minHeight: "100vh", width: "100%" }}>
+        <CssBaseline />
+        <Box sx={{ display: "flex" }}>
+          <AppBar
+            position="fixed"
+            sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          >
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" noWrap component="div">
+                Up2date Family Salon
+              </Typography>
+            </Toolbar>
+          </AppBar>
+
+          <AppDrawer open={openDrawer} onClose={handleDrawerToggle} />
+
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              p: 3,
+              mt: "64px",
+              minHeight: "calc(100vh-64px)",
+            }}
+          >
+            <Routes>
+              <Route
+                path="/"
+                element={<Navigate replace to="/customer-details" />}
+              />
+              <Route path="/customer-details" element={<CustomerPage />} />
+              <Route path="/dashboard" element={<DashboardPage />} />
+            </Routes>
+          </Box>
+        </Box>
+      </Box>
+    </BrowserRouter>
   );
 }
 
