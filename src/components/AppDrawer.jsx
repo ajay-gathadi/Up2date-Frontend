016fc03ae1aa05602aaa4196af6288b7ea37a1ae
Ajay Gathadi <ajaygathadi@gmail.com>
2025-06-14 10:05:13 +0530

    Remove the Drawer component as it is unnecessary

--- src/components/AppDrawer.jsx
+++ src/components/AppDrawer.jsx
@@ -1,58 +1,49 @@
-import { Dashboard, People } from "@mui/icons-material";
-import {
-  Drawer,
-  Box,
-  List,
-  ListItem,
-  ListItemText,
-  Divider,
-  ListItemButton,
-  ListItemIcon,
-} from "@mui/material";
-import { Link } from "react-router-dom";
+import {Dashboard, People} from "@mui/icons-material";
+import {Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText,} from "@mui/material";
+import {Link} from "react-router-dom";
 
-const AppDrawer = ({ open, onClose }) => {
-  const drawerItems = [
-    {
-      text: "Customer Information",
-      icon: <People />,
-      path: "/customer-details",
-    },
-    { text: "Dashboard", icon: <Dashboard />, path: "/dashboard" },
-  ];
+const AppDrawer = ({open, onClose}) => {
+    const drawerItems = [
+        {
+            text: "Customer Information",
+            icon: <People/>,
+            path: "/customer-details",
+        },
+        {text: "Dashboard", icon: <Dashboard/>, path: "/dashboard"},
+    ];
 
-  return (
-    <Drawer anchor="left" open={open} onClose={onClose}>
-      <Box
-        sx={{ width: 245, paddingTop: '32px' }}
-        role="presentation"
-        onClick={onClose}
-        onKeyDown={onClose}
-      >
-        <List>
-          <ListItem>
-            <ListItemText
-              // primary="Menu"
-              slotProps={{
-                primary: { variant: "h6", align: "center" },
-              }}
-            />
-          </ListItem>
-          <Divider />
-          {drawerItems.map((currentItem) => (
-            <ListItem key={currentItem.text} disablePadding>
-              <ListItemButton component={Link} to={currentItem.path}>
-                <ListItemIcon sx={
-                  {color: 'rgba(223, 168, 18, 0.69)'}
-                }>{currentItem.icon}</ListItemIcon>
-                <ListItemText primary={currentItem.text}/>
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Box>
-    </Drawer>
-  );
+    return (
+        // <Drawer anchor="left" open={open} onClose={onClose}>
+        <Box
+            sx={{width: 245, paddingTop: '32px'}}
+            role="presentation"
+            onClick={onClose}
+            onKeyDown={onClose}
+        >
+            <List>
+                <ListItem>
+                    <ListItemText
+                        // primary="Menu"
+                        slotProps={{
+                            primary: {variant: "h6", align: "center"},
+                        }}
+                    />
+                </ListItem>
+                <Divider/>
+                {drawerItems.map((currentItem) => (
+                    <ListItem key={currentItem.text} disablePadding>
+                        <ListItemButton component={Link} to={currentItem.path}>
+                            <ListItemIcon sx={
+                                {color: 'rgba(223, 168, 18, 0.69)'}
+                            }>{currentItem.icon}</ListItemIcon>
+                            <ListItemText primary={currentItem.text}/>
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+        // </Drawer>
+    );
 };
 
 export default AppDrawer;
